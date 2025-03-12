972be85080354276ec65b13704bf30156165ca63
William Quayson <wgsquayson@gmail.com>
2025-03-11 23:17:30 -0300

    chore: add progress circle to card

--- src/routes/Home/components/Card/index.tsx
+++ src/routes/Home/components/Card/index.tsx
@@ -4,6 +4,7 @@ import { useStyle } from "@ui/hooks";
 import { Text } from "@ui/components";
 
 import { CardProps } from "./model";
+import ProgressCircle from "../ProgressCircle";
 
 export default function Card({ pokemon }: CardProps) {
   const styles = useStyle((theme) => ({
@@ -17,18 +18,25 @@ export default function Card({ pokemon }: CardProps) {
       width: 200,
       height: 200,
     },
+    progressCircle: {
+      position: "absolute",
+      right: theme.spacing.sml,
+      top: theme.spacing.sml,
+    },
   }));
 
   return (
     <View style={styles.container}>
+      <View style={styles.progressCircle}>
+        <ProgressCircle progress={120} />
+      </View>
       <Image
         source={{
           uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
         }}
         style={styles.image}
       />
-
-      <Text variant="heading">Bulbassaur</Text>
+      <Text variant="highlight">Bulbassaur</Text>
     </View>
   );
 }
