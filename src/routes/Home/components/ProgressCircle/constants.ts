1e82e713765c2fdd3f9216adf7c071755e0011c3
William Quayson <wgsquayson@gmail.com>
2025-03-11 23:17:10 -0300

    feat: progress circle component

---
+++ src/routes/Home/components/ProgressCircle/constants.ts
@@ -0,0 +1,3 @@
+export const SIZE = 60;
+export const STROKE_WIDTH = 7;
+export const MAX_PROGRESS = 563;
---
+++ src/routes/Home/components/ProgressCircle/index.tsx
@@ -0,0 +1,89 @@
+import {
+  AnimatedProp,
+  Canvas,
+  Group,
+  Path,
+  Skia,
+  SkPoint,
+  Transforms3d,
+} from "@shopify/react-native-skia";
+import { useEffect } from "react";
+import { View } from "react-native";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+
+import { useStyle } from "@ui/hooks";
+import { Text } from "@ui/components";
+
+import { ProgressCircleProps } from "./model";
+import { MAX_PROGRESS, SIZE, STROKE_WIDTH } from "./constants";
+
+export default function ProgressCircle({ progress = 0 }: ProgressCircleProps) {
+  const currentProgress = useSharedValue(0);
+
+  const styles = useStyle(() => ({
+    container: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    canvas: {
+      width: SIZE,
+      height: SIZE,
+    },
+    text: {
+      position: "absolute",
+    },
+  }));
+
+  const origin: AnimatedProp<SkPoint> = {
+    x: SIZE / 2,
+    y: SIZE / 2,
+  };
+
+  const transform: AnimatedProp<Transforms3d> = [
+    {
+      rotate: -Math.PI / 2,
+    },
+  ];
+
+  const radius = SIZE / 2 - STROKE_WIDTH / 2;
+
+  const path = Skia.Path.Make();
+
+  path.addCircle(SIZE / 2, SIZE / 2, radius);
+
+  useEffect(() => {
+    currentProgress.value = withTiming(progress / MAX_PROGRESS, {
+      duration: 1000,
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Canvas style={styles.canvas}>
+        <Group origin={origin} transform={transform}>
+          <Path
+            end={1}
+            start={0}
+            path={path}
+            style="stroke"
+            strokeCap="round"
+            color={styles.theme.color.border.inactive}
+            strokeWidth={STROKE_WIDTH}
+          />
+          <Path
+            start={0}
+            path={path}
+            end={currentProgress}
+            style="stroke"
+            strokeCap="round"
+            color={styles.theme.color.border.active}
+            strokeWidth={STROKE_WIDTH}
+          />
+        </Group>
+      </Canvas>
+      <View style={styles.text}>
+        <Text variant="detail">{String(progress)}xp</Text>
+      </View>
+    </View>
+  );
+}
